f76561e1648ae4fc2651e5193d744ad4583a8ae8
Andy Lindsay <andydlindsay@gmail.com>
2022-06-03 14:16:29 -0700

    breakout over

---
+++ breakout-express-router/router/routers/product-router.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const productQueries = require('../db/product-queries');
+
+router.use((req, res, next) => {
+  // if (!req.cookies.userId) {
+  //   return res.redirect('/login');
+  // }
+  console.log('inside middleware in product router');
+  next();
+});
+
+// GET /products/
+router.get('/', (req, res) => {
+  productQueries.getProducts()
+    .then((products) => {
+      res.json(products);
+    });
+});
+
+// GET /products/:id
+router.get('/:id', (req, res) => {
+  productQueries.getProductById(req.params.id)
+    .then((product) => {
+      res.json(product);
+    });
+});
+
+
+module.exports = router;
--- breakout-express-router/router/server.js
+++ breakout-express-router/router/server.js
@@ -8,6 +8,18 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 
+// require the db
+const db = require('./db/db');
+
+// import the routers
+const productRouter = require('./routers/product-router');
+const blogpostRouterFn = require('./routers/blogpost-router');
+
+// tell Express to use the routers
+app.use('/api/admin/products', productRouter);
+const blogpostRouter = blogpostRouterFn(db);
+app.use('/blogposts', blogpostRouter);
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
